bf6f78f0d76ba26f062257614f0ba2d8555e2412
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-03-02 18:02:59 +0800

    Integrated components library

--- packages/auth/src/components/Login.tsx
+++ packages/auth/src/components/Login.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
+import { Button } from '@little-rock/components';
 
-export default () => (<h1>Login Page</h1>);
+export default () => (
+  <div>
+    <h1>Login Page</h1>
+    <Button label="Primary Button" onClick={() => alert('Primary button is clicked')}/>
+  </div>
+);
--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
+import { IconButton } from '@little-rock/components';
 
-export default () => (<h1>Home Page...</h1>);
+export default () => (
+  <div>
+    <h1>Home Page...</h1>
+    <IconButton label='Primary Icon Button' onClick={() => alert('Primary icon button is clicked')} />
+  </div>
+);
