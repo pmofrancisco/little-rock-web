83f9f43c468df4304ce534e739f630d0a377fc1f
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-03-03 13:18:42 +0800

    Updated login text

--- packages/auth/src/components/Login.tsx
+++ packages/auth/src/components/Login.tsx
@@ -3,7 +3,7 @@ import { Button } from '@little-rock/components';
 
 export default () => (
   <div>
-    <h1>Login Page</h1>
+    <h1>Login</h1>
     <Button label="Primary Button" onClick={() => alert('Primary button is clicked')}/>
   </div>
 );
