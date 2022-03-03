f4ea2579ccc22c7a3d7f13ae5af4b2b25bf0d9e3
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-03-03 13:21:02 +0800

    Updated test

--- packages/auth/src/components/Login.test.tsx
+++ packages/auth/src/components/Login.test.tsx
@@ -5,6 +5,6 @@ import Login from './Login';
 
 test('renders login page', () => {
   const { getByText } = render(<Login />);
-  const h1 = getByText(/login page/i);
+  const h1 = getByText(/login/i);
   expect(h1).toBeInTheDocument();
 });
