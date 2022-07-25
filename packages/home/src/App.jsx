f9f139c39c0d737bdb110af9bbce5c91f4da6013
Paul Michael Francisco <paul.francisco@pdax.ph>
2022-07-25 09:03:58 +0800

    Uninstall little rock components

--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { IconButton } from '@little-rock/components';
+import React from "react";
 
 export default () => (
   <div>
     <h1>Home Page...</h1>
-    <IconButton label='Primary Icon Button' onClick={() => alert('Primary icon button is clicked')} />
   </div>
 );
