d5f8df956bb01cbf0e2610a188a6fe0ece984c34
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-10-07 08:18:12 +0800

    Update home text

--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -3,7 +3,7 @@ import { IconButton } from '@little-rock/components';
 
 export default () => (
   <div>
-    <h1>Home Page</h1>
+    <h1>Home Page 1st Update</h1>
     <IconButton label='Primary Icon Button' onClick={() => alert('Primary icon button is clicked')} />
   </div>
 );
