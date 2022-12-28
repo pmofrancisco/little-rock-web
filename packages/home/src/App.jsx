a91daf3ea77f5e8bb366305485852de52925074d
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-12-28 19:23:12 +0800

    Read excel

--- packages/home/src/App.jsx
+++ packages/home/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef} from 'react';
+import readXlsxFile from 'read-excel-file';
 
-export default () => (
-  <div>
-    <h1>Home Page New S3 Bucket.!@#</h1>
-  </div>
-);
+export default () => {
+  const fileEl = useRef(null);
+  return (
+    <div>
+      <h1>Home Page New S3 Bucket.!@#</h1>
+      <input ref={fileEl} type="file" onChange={() => {
+        const { files } = fileEl?.current;
+        readXlsxFile(fileEl?.current?.files[0]).then((rows) => {
+          console.log({ rows });
+        }).catch((err) => console.log({ err }));
+      }} />
+    </div>
+  );
+}
